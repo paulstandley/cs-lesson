922c08ee79532c65838634af3d948b00aa991c89
Paul Standley <paulstandley1972@gmail.com>
2019-04-23 19:04:00 +0100

    Hello World

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "hello";
+            string world = "world";
+            Console.WriteLine(hello + " " + world);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cs-lesson
+# Hello World
+
+## _C#_
+
+---
+
+![Profile Pick](http://res.cloudinary.com/pieol2/image/upload/v1516543296/profile-small.png)
+
+## *_Paul Standley_*
+
+---
